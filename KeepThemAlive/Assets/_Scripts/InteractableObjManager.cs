0b9cfab0297e0bd34bee08ff63c908b92ecbed2c
Unknown <leo11leo55@yahoo.com>
2018-03-02 17:00:11 +0000

    Sporting 3-0 Porto
    REGO NA MEXE
    Ao clicar no E consegue mover caixas, e chamar npc
    (npc ainda nao andam)

---
+++ KeepThemAlive/Assets/_Scripts/InteractableObjManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjManager : MonoBehaviour {
+
+    public static GameObject[] Boxes;
+    public static GameObject[] NPCs;
+
+    void Start ()
+    {
+        Boxes = GameObject.FindGameObjectsWithTag("Boxes");
+        NPCs = GameObject.FindGameObjectsWithTag("NPC");
+
+    }
+	
+
+	void Update ()
+    {
+		
+	}
+}
---
+++ KeepThemAlive/Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 moveVelocity;
+    private Vector3 moveInput;
+    private float forwardAmount;
+    public float turnAmount;
+    public float moveSpeed;
+    bool canMove;
+    bool IsGrabed;
+    Rigidbody rb;
+    public Camera mainCamera;
+
+    // Use this for initialization
+    void Start ()
+    {
+        canMove = true;
+        IsGrabed = false;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (canMove)
+        {
+            rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
+            MovementTopDown();
+            ConvertMoveInput();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PushBoxes();
+            SurvivalFollow();
+        }
+    }
+
+    void SurvivalFollow()
+    {
+        GameObject[] npcs = InteractableObjManager.NPCs;
+        GameObject NewNPC = null;
+        float distance;
+
+        foreach (GameObject NPC in npcs)
+        {
+            distance = Vector3.Distance(transform.position, NPC.transform.position);
+
+            if (distance < 1.5f)
+            {
+                NewNPC = NPC;
+                Debug.Log("Este é o idiota: " + NewNPC.name);
+                break;
+            }
+            else
+                distance = 0.0f;
+
+        }
+    }
+
+    void PushBoxes()
+    {
+        float distance;
+        GameObject BoxGO = null;
+        GameObject[] box = InteractableObjManager.Boxes;
+        
+        if (!IsGrabed)
+        {
+            // vê se tem alguma caixa perto
+            foreach (GameObject caixa in box)
+            {
+                distance = Vector3.Distance(transform.position, caixa.transform.position);
+
+                if (distance < 1.5f)
+                {
+                    BoxGO = caixa;
+                    IsGrabed = true;
+                    break;
+                }
+                else
+                    distance = 0.0f;
+                
+            }
+
+            if (BoxGO != null)
+                BoxGO.transform.parent = this.transform;
+        }
+        else
+        {
+            transform.GetChild(0).parent = null;
+            IsGrabed = false;
+        }
+
+    }
+
+    void ConvertMoveInput()
+    {
+        Vector3 localMove = transform.InverseTransformDirection(moveInput);
+        turnAmount = localMove.x;
+        forwardAmount = localMove.z;
+    }
+
+    void MovementTopDown()
+    {
+        {
+            moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+            moveInput = transform.TransformDirection(moveInput);
+
+            moveVelocity = moveInput * moveSpeed;
+
+            Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+            float rayLenght;
+
+            if (groundPlane.Raycast(cameraRay, out rayLenght))
+            {
+                Vector3 pointToLook = cameraRay.GetPoint(rayLenght);
+                Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            }
+        }
+    }
+}

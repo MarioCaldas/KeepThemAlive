3ec336c7cecf66f7db0de82bb253f13d8931af62
Unknown <leo11leo55@yahoo.com>
2018-03-16 15:47:26 +0000

    Camera Zoom
    Camera já faz zoom corretamente e melhorei o código de rotação. (Tudo funcional)

--- KeepThemAlive/Assets/CamRotation.cs
+++ KeepThemAlive/Assets/CamRotation.cs
@@ -11,6 +11,7 @@ public class CamRotation : MonoBehaviour {
     public GameObject targetObject;
     private float targetAngleX = 0;
     private float targetAngleY = 0;
+    private float Zooming = 0;
 
     const float rotationAmount = 1.5f;
     const float rotationAmountY = 1.5f;
@@ -18,10 +19,10 @@ public class CamRotation : MonoBehaviour {
     public float rSpeed = 1.0f;
 
     bool isRotatingX = false;
-
     bool isRotatingY = false;
 
-
+    int ZoomMode = 0;
+    int UpDownMode = 0;
 
 
     void Start () {
@@ -35,121 +36,141 @@ public class CamRotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && !isRotatingX && !isRotatingY)
-        {
-            Debug.Log("ESQUERDA");
+        #region INPUTS
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && !isRotatingX && !isRotatingY)
+            {
+                Debug.Log("ESQUERDA");
           
-            if (transform.forward != -Vector3.up)
-            {
-                targetAngleX += 91.5f;
-                isRotatingX = true;
+                if (transform.forward != -Vector3.up)
+                {
+                    targetAngleX += 91.5f;
+                    isRotatingX = true;
+                }
             }
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && !isRotatingX && !isRotatingY)
-        {
-            Debug.Log("DIREITA");
-
-            if (transform.forward != -Vector3.up)
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && !isRotatingX && !isRotatingY)
             {
-                targetAngleX -= 91.5f;
-                isRotatingX = true;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && !isRotatingY && !isRotatingX)
-        {
-            Debug.Log("UP");
+                Debug.Log("DIREITA");
 
-            if (transform.forward != -Vector3.up)
+                if (transform.forward != -Vector3.up)
+                {
+                    targetAngleX -= 91.5f;
+                    isRotatingX = true;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow) && !isRotatingY && !isRotatingX)
             {
-                targetAngleY -= 91.5f;
-                isRotatingY = true;
-            }
-            
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && !isRotatingY && !isRotatingX)
-        {
-            Debug.Log("DOWN");
+                Debug.Log("UP");
 
-            if (transform.forward == -Vector3.up)
+                if (ZoomMode == 0)
+                {
+                    if (UpDownMode == 0)
+                    {
+                        targetAngleY -= 91.5f;
+                        isRotatingY = true;
+                        UpDownMode++;
+                    }
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && !isRotatingY && !isRotatingX)
             {
-                targetAngleY += 91.5f;
-                isRotatingY = true;
-            }
-        }
+                Debug.Log("DOWN");
 
-        if(targetAngleX == -1.5f)
+                if (ZoomMode == 0)
+                {
+                    if (UpDownMode == 1)
+                    {
+                        targetAngleY += 91.5f;
+                        isRotatingY = true;
+                        UpDownMode--;
+                    }
+                }
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                if (ZoomMode == 0 && UpDownMode == 0)
+                {
+                    Zooming = 100;
+                    ZoomMode++;
+                }
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                if (ZoomMode == 1)
+                {
+                    Zooming = -100;
+                    ZoomMode--;
+                }
+            }
+        #endregion
+
+        if (targetAngleX == -1.5f || targetAngleX == 1.5f)
         {
             isRotatingX = false;
             targetAngleX = 0;
         }
-        if (targetAngleX == 1.5f)
-        {
-            isRotatingX = false;
-            targetAngleX = 0;
 
-        }
-
-        if (targetAngleY == 1.5f)
+        if (targetAngleY == 1.5f || targetAngleY == -1.5f)
         {
             isRotatingY = false;
             targetAngleY = 0;
         }
-        if (targetAngleY == -1.5f)
-        {
 
-            isRotatingY = false;
-            targetAngleY = 0;
+        if (Zooming != 0)
+        {
+            Zoom();
         }
 
-
-        if (targetAngleX != 0)
+        if (targetAngleX != 0 || targetAngleY != 0)
         {
-            Debug.Log(isRotatingX);
-
             if (isRotatingX)
             {
                 Rotate();
             }
-        }
-
-
-        if (targetAngleY != 0)
-        {
-            if (isRotatingY)
+            else if (isRotatingY)
             {
                 Rotate();
             }
         }
     }
 
+    
     protected void Rotate()
     {
         if (targetAngleX > 0)
         {
             transform.RotateAround(targetObject.transform.position, transform.up, rotationAmount);
             targetAngleX -= rotationAmount;
-            
-
         }
         else if (targetAngleX < 0)
         {
             transform.RotateAround(targetObject.transform.position, transform.up, -rotationAmount);
             targetAngleX += rotationAmount;
         }
-
         else if (targetAngleY < 0)
         {
-
             transform.RotateAround(targetObject.transform.position, transform.right, rotationAmountY);
             targetAngleY += rotationAmountY;
         }
         else if(targetAngleY >= 0)
         {
-            Debug.Log("boi");
-
             transform.RotateAround(targetObject.transform.position, transform.right, -rotationAmountY);
             targetAngleY -= rotationAmountY;
+        }
+    }
 
+    protected void Zoom()
+    {
+        if (Zooming > 0)
+        {
+            // ZOOM IN
+            transform.position += transform.forward * 2;
+            Zooming -= 2f;
+        }
+        else if (Zooming < 0)
+        {
+            // ZOOM OUT
+            transform.position -= transform.forward * 2;
+            Zooming += 2f;
         }
     }
 }

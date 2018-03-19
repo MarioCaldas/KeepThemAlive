733a793cf4ee5bf75d3b39fe22bb8a09046df110
Unknown <caldas_96@live.com.pt>
2018-03-19 17:37:03 +0000

    ChangeViewAndSubtituteObjects
    fase 1 - desk and window

---
+++ KeepThemAlive/Assets/_Scripts/ChangeObjects.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeObjects : MonoBehaviour
+{
+    public Button substituteButton;
+    string objectTag = "Object";
+
+    string deskTag = "Desk";
+    string windowTag = "Window";
+
+    public Camera mainCamera;
+
+	void Start ()
+    {
+        Button subButton = substituteButton.GetComponent<Button>();
+        subButton.onClick.AddListener(SubstituteObject);
+
+        mainCamera = Camera.main;
+	}
+
+    void SubstituteObject()
+    {
+     
+
+    }
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                ObjectClicked(hit);
+            }
+        }
+    }
+
+    void ObjectClicked(RaycastHit hit)
+    {
+        if(hit.transform.tag == deskTag)
+        {
+            Debug.Log("Desk seu fffffffilho da puta");
+        }
+
+        else if(hit.transform.tag == windowTag)
+        {
+            Debug.Log("Windoooooooooh");
+        }
+    }
+}
---
+++ KeepThemAlive/Assets/_Scripts/ChangeView.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeView : MonoBehaviour {
+
+    public Button buttonIntView;
+    public Button buttonExtView;
+
+    public GameObject roof;
+
+    public GameObject Cam;
+
+    private CamRotation camRotScript;
+
+	void Start ()
+    {
+        Button intButton = buttonIntView.GetComponent<Button>();
+        Button extButton = buttonExtView.GetComponent<Button>();
+
+        intButton.onClick.AddListener(ChangeIntView);
+        extButton.onClick.AddListener(ChangeExtView);
+
+
+        camRotScript = Cam.GetComponent<CamRotation>();
+
+    }
+	
+    void ChangeIntView()
+    {
+        roof.SetActive(false);
+
+        camRotScript.targetAngleY -= 91.5f;
+        camRotScript.isRotatingY = true;
+        camRotScript.UpDownMode++;
+    }
+
+    void ChangeExtView()
+    {
+        roof.SetActive(true);
+
+        camRotScript.targetAngleY += 91.5f;
+        camRotScript.isRotatingY = true;
+        camRotScript.UpDownMode--;
+
+    }
+
+
+    void Update ()
+    {
+		
+	}
+}
